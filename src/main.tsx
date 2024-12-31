26668335a18a97024041960ccef53823a315fa6e
Wei722 <thewei722@gmail.com>
2025-01-01 01:09:36 +1100

    Update configuration files for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+const manifestUrl = 'https://Wei722.github.io/my-test/tonconnect-manifest.json';
+
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
